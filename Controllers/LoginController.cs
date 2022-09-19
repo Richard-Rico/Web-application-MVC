e652b995496d4349fbb42d442f8e5853fdc99ffc
Richard-Rico <104793974+Richard-rico@users.noreply.github.com>
2022-09-19 11:12:12 +0300

    Files project

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using WebAppFirstMVC.Models;
+
+namespace WebAppFirstMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(Logins LoginModel)
+        {
+            NorthwindOriginalEntities db = new NorthwindOriginalEntities();
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.UserName == LoginModel.UserName && x.PassWord == LoginModel.PassWord); //Haetaan käyttäjän/Loginin tiedot annetuilla tunnustiedoilla tietokannasta LINQ -kyselyllä
+
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                ViewBag.LoggedStatus = "In";
+                Session["UserName"] = LoggedUser.UserName;
+                return RedirectToAction("Index", "Home"); //Tässä määritellään mihin onnistunut kirjautuminen johtaa --> Home/Index
+
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.LoggedStatus = "Out";
+                LoginModel.LoginErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+
+                return View("Login", LoginModel);
+            }
+
+
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Home");
+        }
+
+        //public ActionResult LoggedOut()
+        //{
+        //    ViewBag.LoggedOut = "Olet kirjautunut ulos järjestelmästä.";
+        //    return View(); //We have a special wiev LoggedOut which is a copy of Index, but has additional viewbag message of
+        //                   //succesfull logout and possibility to login again.
+        //}
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppFirstMVC.Models;
+
+namespace WebAppFirstMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        NorthwindOriginalEntities db = new NorthwindOriginalEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                NorthwindOriginalEntities db = new NorthwindOriginalEntities();
+                List<Products> model = db.Products.ToList();
+                db.Dispose();
+                return View(model);
+            }
+        }
+    }
+}
